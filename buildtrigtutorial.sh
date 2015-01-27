3d05d16103599614db110878983f2406236c1e48
Gordon <gordon@gordonandpaula.net>
2015-01-27 21:31:00 +0100

    Update for jan version of tutorial

--- buildtrigtutorial.sh
+++ buildtrigtutorial.sh
@@ -4,12 +4,15 @@
 #  Should be run from the directory where it is located.
 #
 
-cp experiment/packages.txt distribution/packages.txt
-cp experiment/trigTutTest/cmt/Makefile.RootCore distribution
-cp experiment/trigTutTest/util/simpleshell.cxx distribution
-cp experiment/trigTutTest/util/trigDecsion.cxx distribution
-cp experiment/trigTutTest/util/allTriggers.cxx distribution
-cp experiment/trigTutTest/util/associatedFeatures.cxx distribution
+# Commented out because latest stuff has us building
+# directly from distribution area rather than other way
+# around.
+#cp experiment/packages.txt distribution/packages.txt
+#cp experiment/trigTutTest/cmt/Makefile.RootCore distribution
+#cp experiment/trigTutTest/util/simpleshell.cxx distribution
+#cp experiment/trigTutTest/util/trigDecsion.cxx distribution
+#cp experiment/trigTutTest/util/allTriggers.cxx distribution
+#cp experiment/trigTutTest/util/associatedFeatures.cxx distribution
 
 # Zip it up ready for upload
-tar -czf triggerTutorialFilesDec2014.tar.gz distribution/*
+tar -czf triggerTutorialFilesJan2015.tar.gz distribution/*
